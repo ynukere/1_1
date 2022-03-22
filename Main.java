7648d3ff82926eeb9541d91fbd100c24a14525df
yzumite <101186651+ynukere@users.noreply.github.com>
2022-03-22 11:26:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("H");
+    System.out.println("e");
+    System.out.println("l");
+    System.out.println("l");
+    System.out.println("o");
+    System.out.println(" ");
+    System.out.println("W");
+    System.out.println("o");
+    System.out.println("r");
+    System.out.println("l");
+    System.out.println("d");
+    System.out.println("!");
+  }
+}
